0ab82514741e60b943bb5605d0408aef33ababc6
MwitaBrian <brianmwita13@gmail.com>
2023-02-19 06:04:52 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age , sex, color, temperament, alive)
+VALUES ('Mr.Chocolate', 25, 'M', 'White', 'Choleric', true),
+       ('Rowdy', 22, 'M', 'Brown', 'Sanguine', false ),
+       ('Tabitha', 12, 'M', 'White', 'Phlegmatic', true ),
+       ('Sergeant Brown', 8, 'F', 'White', 'Choleric', false ),
+       ('Melissa', 9, 'M', 'Brown', 'Melancholic', true ),
+       ('Grinch', 15, 'F', 'White', 'Sanguine', false ),
+       ('Wendy', 10, 'M', 'White', 'Sanguine', true ),
+       (NULL, 5, 'F', 'White', 'Phlegmatic', true );
